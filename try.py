41ee62de93bb267639d80ae71cdd21d7fdb2e209
Kundansingh Mourya <kundanmourya04@gmail.com>
2022-08-27 17:31:36 +0530

    Delete try.py

--- try.py
+++
@@ -1,9 +0,0 @@
-import bz2file as bz2
-import pickle
-
-def decompress_pickle(file):
-    data = bz2.BZ2File(file, 'rb')
-    data = pickle.load(data)
-    return data
-
-model = decompress_pickle('similarity.pbz2')
